705bdcd0bcef37cfdc5c2f808d5bb64fd88dd0d4
SteTisci <ste.tisci@gmail.com>
2024-10-12 19:03:55 +0200

    aggiunto link GET per salvataggio elementi aggiunti al carrello

--- src/products.php
+++ src/products.php
@@ -3,7 +3,6 @@
 $data = json_decode(file_get_contents('https://dummyjson.com/products/category/mobile-accessories'), true);
 $items = $data['products'];
 
-
 foreach ($items as $item) {
     echo '<div class="product-card">';
     echo '<div class="image-container">';
@@ -13,6 +12,8 @@ foreach ($items as $item) {
     echo '<p class="name">' . htmlspecialchars($item['title']) . '</p>';
     echo '<p class="price">' . htmlspecialchars($item['price']) . " €" . '</p>';
     echo '</div>';
-    echo '<button class="add">Add to cart</button>';
+    echo '<div class="submit">';
+    echo '<a href="index.php?title=' . htmlspecialchars($item['title']) . '&price=' . htmlspecialchars($item['price']) . '&thumbnail=' . htmlspecialchars($item['thumbnail']) . '"><button class="add">Add to cart</button></a>';
+    echo '</div>';
     echo '</div>';
 }
