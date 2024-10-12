51b71620c1a4f292eb1d77eed93aa3a81a8562b2
SteTisci <ste.tisci@gmail.com>
2024-10-12 19:04:20 +0200

    controllo prodotti e aggiunta in sessione

--- src/script.php
+++ src/script.php
@@ -1,2 +1,36 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+
+    if (!isset($_SESSION['items'])) {
+        $_SESSION['items'] = [];
+    }
+
+    if (isset($_GET['title']) && !empty($_GET['title'])) {
+        $found = false;
+
+        // Searches if an item is already present in the session and increases its quantity
+        foreach ($_SESSION['items'] as &$item) {
+            if ($item['title'] == $_GET['title']) {
+                $item['quantity']++;
+                $found = true;
+                break;
+            }
+        }
+
+        // If the item is not present, add a new one to the session
+        if (!$found) {
+            $_SESSION['items'][] = [
+                'title' => $_GET['title'],
+                'price' => $_GET['price'],
+                'thumbnail' => $_GET['thumbnail'],
+                'quantity' => 1,
+            ];
+        }
+
+        // Redirection to solve the double submit problem (PRG pattern)
+        header('location: index.php');
+        exit();
+    }
+}
+
