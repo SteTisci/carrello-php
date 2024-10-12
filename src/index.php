d5288e2b5dbb021648f6e8bb3c697795fa2f40ec
SteTisci <ste.tisci@gmail.com>
2024-10-12 19:03:28 +0200

    body intergrato in index

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php session_start() ?>
 <html lang="en">
 
 <head>
@@ -6,8 +7,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
     <title>Shopping Cart</title>
+
+    <?php include "script.php" ?>
 </head>
 
-<?php include 'body.php' ?>
+<body>
+    <div class="wrapper">
+
+        <section class="header">
+            <div class="logo">
+                <img src="../images/logo.svg" width="40px" height="auto">
+                <h1 class="title">Shopping Cart</h1>
+            </div>
+
+            <div class="cart-button">
+                <a href="checkout.php">
+                    <img src="../images/cart.svg" width="25px" height="auto">
+                    <p class="products-quantity"><?php echo count($_SESSION['items']) ?></p>
+                </a>
+            </div>
+        </section>
+
+        <main class="content">
+            <?php include 'products.php' ?>
+        </main>
+
+    </div>
+</body>
 
 </html>
